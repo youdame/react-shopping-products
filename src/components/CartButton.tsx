facabffb6c4f75055fb877dbcc76ce4e94b83d48
spoyodevelop <kjy9505@gmail.com>
2025-05-14 14:43:35 +0900

    feat: 카트 버튼 구현 Co-authored-by: youdame <112458620+youdame@users.noreply.github.com>

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -1,6 +1,9 @@
 import { css } from "@emotion/react";
-interface CartButtonProps {
+import { ComponentProps } from "react";
+import useFetch from "../hooks/useFetch";
+interface CartButtonProps extends ComponentProps<"button"> {
   isInCart: boolean;
+  onClick: () => void;
 }
 const buttonCss = css({
   padding: "4px 8px",
@@ -18,26 +21,56 @@ const buttonCss = css({
 });
 
 const inCart = css({
-  backgroundColor: "black",
-  color: "white",
-});
-const notInCart = css({
   backgroundColor: "#EAEAEA",
   color: "black",
 });
+const notInCart = css({
+  backgroundColor: "black",
+  color: "white",
+});
 
-export default function CartButton({ isInCart }: CartButtonProps) {
+export default function CartButton({
+  isInCart,
+  onClick,
+  ...props
+}: CartButtonProps) {
+  const { fetcher: addCartItem } = useFetch(
+    "http://techcourse-lv2-alb-974870821.ap-northeast-2.elb.amazonaws.com/cart-items",
+    {
+      headers: {
+        Authorization: `Basic ${btoa(
+          `${import.meta.env.VITE_USER_ID}:${import.meta.env.VITE_PASSWORD}`
+        )}`,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        productId: 117,
+        quantity: 1,
+      }),
+    },
+    false
+  );
+  const handleButtonClick = () => {
+    addCartItem();
+    onClick();
+  };
+
   return (
-    <button css={[buttonCss, isInCart ? inCart : notInCart]}>
+    <button
+      {...props}
+      onClick={handleButtonClick}
+      css={[buttonCss, isInCart ? inCart : notInCart]}
+    >
       {isInCart ? (
-        <>
-          <img src="assets/filledCart.svg" />
-          <span>담기</span>
-        </>
-      ) : (
         <>
           <img src="assets/emptyCart.svg" />
           <span>빼기</span>
+        </>
+      ) : (
+        <>
+          <img src="assets/filledCart.svg" />
+          <span>담기</span>
         </>
       )}
     </button>
