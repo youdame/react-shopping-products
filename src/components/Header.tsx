dc1ca36a53529ac66b2808c88b1fff19ed63bd9f
spoyodevelop <kjy9505@gmail.com>
2025-05-14 14:42:56 +0900

    feat: 헤더 구현 Co-authored-by: youdame <112458620+youdame@users.noreply.github.com>

---
+++ src/components/Header.tsx
@@ -0,0 +1,51 @@
+import { css } from "@emotion/react";
+const header = css({
+  position: "fixed",
+  top: "0%",
+  left: "50%",
+  transform: "translate(-50%, 0)",
+  height: "64px",
+  width: "382px",
+  backgroundColor: "black",
+  padding: "24px",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  p: { color: "white", fontWeight: 800, fontSize: "20px" },
+});
+
+const cartIcon = css({
+  position: "relative",
+
+  img: {
+    width: "32px",
+    height: "32px",
+  },
+  span: {
+    position: "absolute",
+    width: "18px",
+    height: "18px",
+    borderRadius: "50%",
+    backgroundColor: "white",
+    bottom: "0%",
+    right: "0%",
+    textAlign: "center",
+    fontSize: "0.8rem",
+  },
+});
+interface headerProps {
+  cartLength: number;
+}
+function Header({ cartLength }: headerProps) {
+  return (
+    <header css={header}>
+      <p>SHOP</p>
+      <button css={cartIcon}>
+        <img src="assets/cart.svg" alt="cart-icon" />
+        <span>{cartLength}</span>
+      </button>
+    </header>
+  );
+}
+
+export default Header;
