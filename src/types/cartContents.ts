8377f603532994c65b797589d1fda2d36d6d31df
spoyodevelop <kjy9505@gmail.com>
2025-05-14 14:42:07 +0900

    feat: cart, product response type 선언 Co-authored-by: youdame <112458620+youdame@users.noreply.github.com>

---
+++ src/types/cartContents.ts
@@ -0,0 +1,6 @@
+import { Product } from "./product";
+export interface CartItem {
+  id: number;
+  quantity: number;
+  product: Product;
+}
---
+++ src/types/response.ts
@@ -0,0 +1,44 @@
+import { CartItem } from "./cartContents";
+import { Product } from "./product";
+export interface CartItemResponse {
+  content: CartItem[];
+  pageable: Pageable;
+  totalElements: number;
+  totalPages: number;
+  last: boolean;
+  size: number;
+  number: number;
+  sort: Sort;
+  numberOfElements: number;
+  first: boolean;
+  empty: boolean;
+}
+
+export interface ProductResponse {
+  totalElements: number;
+  totalPages: number;
+  size: number;
+  content: Product[];
+  number: number;
+  sort: Sort;
+  pageable: Pageable;
+  numberOfElements: number;
+  first: boolean;
+  last: boolean;
+  empty: boolean;
+}
+
+interface Pageable {
+  offset: number;
+  sort: Sort;
+  paged: boolean;
+  pageNumber: number;
+  pageSize: number;
+  unpaged: boolean;
+}
+
+interface Sort {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
+}
