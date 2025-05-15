1427c1efa28a0dcf249f7cad2b3ab99ab2bb86bc
spoyodevelop <kjy9505@gmail.com>
2025-05-15 11:19:25 +0900

    refactor: Product 컴포넌트 및 스타일 분리 Co-authored-by: youdame <112458620+youdame@users.noreply.github.com>

---
+++ src/components/Product/ProductCard/ProductCard.style.ts
@@ -0,0 +1,28 @@
+import { css } from "@emotion/react";
+
+export const cardCss = css({
+  display: "flex",
+  width: "182px",
+  height: "250px",
+  flexDirection: "column",
+  borderRadius: "8px 8px 0 0",
+  backgroundColor: "white",
+});
+
+export const imageCss = css({
+  objectFit: "cover",
+  borderRadius: "8px 8px 0 0 ",
+  height: "50%",
+});
+
+export const detailCss = css({
+  padding: "15px 8px 0 8px",
+  h2: {
+    fontWeight: "700",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    fontSize: "20px",
+  },
+  p: { fontWeight: "500", fontSize: "14px", marginBottom: "1rem" },
+});
---
+++ src/components/Product/ProductCard/ProductCard.tsx
@@ -0,0 +1,47 @@
+import CartButton from "../../CartButton/CartButton";
+import * as styles from "./ProductCard.style";
+interface ProductCardProps {
+  title: string;
+  price: number;
+  imageUrl: string;
+  onClick: () => Promise<void>;
+  isItemInCart: boolean;
+  productId: number;
+  cartItemId?: number;
+}
+// imageUrl이 null이면, 대체 이미지를 띄웁시다!
+// onLoad라는 프로퍼티가 있는데.. 이것을 어떻게 하면..
+// 이미지가 로드될때, spinnner를 띄울수 있을지도?
+
+function ProductCard({
+  title,
+  price,
+  imageUrl,
+  isItemInCart,
+  onClick,
+  productId,
+  cartItemId,
+}: ProductCardProps) {
+  return (
+    <li css={styles.cardCss}>
+      <img
+        css={styles.imageCss}
+        src={imageUrl}
+        alt={`${title}상품`}
+        onLoad={() => console.log("로드 되었슈!")}
+      />
+      <div css={styles.detailCss}>
+        <h2>{title}</h2>
+        <p>{`${price.toLocaleString()}원`}</p>
+        <CartButton
+          productId={productId}
+          onClick={onClick}
+          isInCart={isItemInCart}
+          cartItemId={cartItemId}
+        />
+      </div>
+    </li>
+  );
+}
+
+export default ProductCard;
