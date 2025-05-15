7cbfca1155ec46c50dbd44c259a27fe8a82407c9
spoyodevelop <kjy9505@gmail.com>
2025-05-15 11:41:24 +0900

    feat: 프로덕트 쿼리스트링 빌드 훅 작성 Co-authored-by: youdame <112458620+youdame@users.noreply.github.com>

---
+++ src/hooks/useProductQuery.ts
@@ -0,0 +1,22 @@
+import { useMemo } from "react";
+import { URLS } from "../constants/url";
+const defaultSearchParams = {
+  page: "0",
+  size: "50",
+};
+const sortParams: Record<OrderByOptionType, string> = {
+  "낮은 가격순": "price,asc",
+  "높은 가격순": "price,desc",
+};
+
+export function useProductQuery(orderBy: OrderByOptionType) {
+  return useMemo(() => {
+    const url = new URL(URLS.PRODUCTS);
+    const params = new URLSearchParams({
+      ...defaultSearchParams,
+      sort: sortParams[orderBy],
+    });
+    url.search = params.toString();
+    return url;
+  }, [orderBy]);
+}
