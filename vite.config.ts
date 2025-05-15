ae5217fe2a2e4d35b0ad12c6314a880631bbdc3e
spoyodevelop <kjy9505@gmail.com>
2025-05-15 15:57:05 +0900

    chore: vitest 셋업 Co-authored-by: youdame <112458620+youdame@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
@@ -7,4 +7,9 @@ export default defineConfig({
       jsxImportSource: "@emotion/react",
     }),
   ],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["./src/setupTests.ts"],
+  },
 });
