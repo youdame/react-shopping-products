598afdd1a0370dab45788131a79b63cbf4ea3711
youdame <112458620+youdame@users.noreply.github.com>
2025-05-23 05:56:37 +0900

    refactor: 헤더 네이밍 변경

--- src/App.style.ts
+++ src/App.style.ts
@@ -1,9 +1,12 @@
 import { css } from '@emotion/react';
 
-//body라기 보다는 , app css 정도로 하면 좋을듯..
 export const bodyCss = css({
   width: '400px',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center'
+});
+
+export const headerHeight = css({
+  height: '80px'
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import * as styles from './App.style';
-import Header from './components/Header/HomeHeader';
+import HomeHeader from './components/Header/HomeHeader';
+
 import ProductList from './components/Product/ProductList/ProductList';
 
 function App() {
   return (
     <div css={styles.bodyCss}>
-      <div style={{ marginBottom: '80px' }} />
-      <Header />
+      <div css={styles.headerHeight} />
+      <HomeHeader />
       <ProductList />
     </div>
   );
